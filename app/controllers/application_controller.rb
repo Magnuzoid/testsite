ef59be2755e1049f45cf6e176314de06ef9b0e86
Magnuzoid <magnus.nielsen@gmail.com>
2016-05-06 11:51:13 +0200

    updating install with new controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
     render text: "¯d”≈¢˜}¯¢”hællo wårrld"
+    test = "hej"
+    #{}"#{test}"
     puts "hej"
+    #test = ('a'..'z').to_a.shuffle[0..7].join
   end
 end
